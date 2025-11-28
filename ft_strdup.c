e7d97be888faa55e5d60433d4583932189a44c44
Carmen Cecilia Gomez Carmona <carmengomezcarmona@gmail.com>
2025-11-28 18:40:14 +0100

    act

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,12 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   prueba.c                                           :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: carmgome <carmgome@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/25 16:11:32 by carmgome          #+#    #+#             */
-/*   Updated: 2025/11/25 16:12:04 by carmgome         ###   ########.fr       */
+/*   Created: 2025/11/28 17:28:00 by carmgome          #+#    #+#             */
+/*   Updated: 2025/11/28 18:01:59 by carmgome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char    *ft_strdup(const char *s1)
+{
+    size_t  len;
+    char    *copy;
+    size_t  i;
+
+    len = ft_strlen(s1);
+    copy = malloc(len + 1);
+
+    if (!copy)
+        return (NULL);
+
+    i = 0;
+    while (i < len)
+    {
+        copy[i] = s1[i];
+        i++;
+    }
+
+    copy[i] = '\0';
+
+    return (copy);
+}
