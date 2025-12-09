31580f72b606cf773ea3f22b2ca7f0eba38a1b23
Carmen Cecilia Gomez Carmona <carmgome@c1r4s5.42malaga.com>
2025-12-09 14:41:23 +0100

    Libft

---
+++ ft_lstadd_front.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmgome <carmgome@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 13:14:44 by carmgome          #+#    #+#             */
+/*   Updated: 2025/12/09 14:40:27 by carmgome         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if (!new || !lst)
+        return;
+    new->next = *lst;
+    *lst = new;
+}
+int main (void)
+{
+    
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carmgome <carmgome@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 11:57:37 by carmgome          #+#    #+#             */
+/*   Updated: 2025/12/09 13:22:18 by carmgome         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
+
+t_list *ft_lstnew(void *content)
+{
+    t_list *new_node;
+
+    new_node = malloc (sizeof(t_list));
+    if (!new_node)
+        return (NULL);
+    new_node -> content = content;
+    new_node -> next = NULL;
+    return (new_node);
+}
+#include <stdio.h>
+int main(void)
+{
+    t_list *nodo;
+    nodo = ft_lstnew("Hola");
+    printf("nodo:%p\n", nodo);
+    printf("este es el contenido del nodo: %s", (char *)nodo->content);
+}
